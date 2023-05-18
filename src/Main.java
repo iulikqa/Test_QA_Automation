2ba97fe005ea60568bf42c1c5349e1353439d17e
Iulian <iulikqa@gmial.com>
2023-05-18 20:03:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Iulian";
+        String surname = "Bujor";
+        String position = "Manua QA";
+        System.out.println("My name is " + name + " " + surname);
+        System.out.println("My function is " + " " + position);
+    }
+}
