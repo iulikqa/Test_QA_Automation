dd8dd3d4c193ec3e9bd5fb896ec14abb32f63edc
Iulian <iulikqa@gmial.com>
2023-05-23 22:00:24 +0300

    Homework_JAVA_Lesson_2

--- src/Homework/Homework_JAVA_Lesson_2.java
+++ src/Homework/Homework_JAVA_Lesson_2.java
@@ -3,14 +3,14 @@ package Homework;
 public class Homework_JAVA_Lesson_2 {
     public static void main(String[] args) {
         int[] company = {9, 12, 5, 44, 3};
-        for (int i = 0; i < company.length; i++) {
-        }
-
         //Посчитать сколько элементов имеет масив сompany(что б не с 0 считал а с 1)
-        System.out.println("Index 1" + " " + "= " + company[1]);
+        int g = 0;
+        for (int i = 1; i < company.length; i++) {
+            g++;
+        }
+        System.out.println("count elements" + " " + g);
 
         //Вывести последний, первый и второй элемент
-
         int lastIndex = company.length - 1;
         int firstElement = company[0];
         int secondElement = company[1];
