ff84556244dd808fb4b3f5ba3f2c65c41ad906b2
Iulian <iulikqa@gmial.com>
2023-06-19 19:19:36 +0300

    Vertex Homework_Vertex_Cycles_Part_III

---
+++ src/Homework/Homework_Vertex_Cycles_Part_II.java
@@ -0,0 +1,23 @@
+package Homework;
+
+public class Homework_Vertex_Cycles_Part_II {
+    public static void main(String[] args) {
+        //Задача №1
+        //Необходимо, чтоб программа выводила на экран вот такую последовательность:
+        //7 14 21 28 35 42 49 56 63 70 77 84 91 98
+        int a = 7;
+        while (a <= 98) {
+            System.out.println(a + " ");
+            a += 7;
+        }
+
+        //Задача №2
+        // Необходимо вывести на консоль такую последовательность чисел:
+        //1 2 4 8 16 32 64 128 256 512
+        int b = 1;
+        while (b <= 512) {
+            System.out.print(b + " ");
+            b *= 2;
+        }
+    }
+}
